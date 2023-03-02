b175059b48d849c7624530c17ff36b159f9db2ca
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-03-01 22:08:39 -0600

    fixed issue with local storage code

--- script.js
+++ script.js
@@ -7,24 +7,19 @@ const handleStore = () => {
     if(!store.length) return;
 
 
-    // fix this 
     store.forEach((task,i) => {
         console.log(i,task);
         $('textarea').eq(i).text(task);
-        // maybe try getItem instead of text
-
     });
 };
 
-handleStore();
-
 
 const hours = ['8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm'];
-
+// You can as many hours as you want and that will produce a new row in our calendar
 hours.forEach((hour, i) => {
     let rH = i+8;
     let cH = new Date().getHours;
-
+// ^ I converted the time to military time 
     main.innerHTML +=`
         <div class="row time-block ${ rH<cH ? 'past' : rH>cH ? 'future' : 'present' }">
         <div class="col-2 col-md-1 hour text-center py-3">${hour}</div>
@@ -33,7 +28,11 @@ hours.forEach((hour, i) => {
         <i class="fas fa-save" aria-hidden="true"></i>
         </button>
     </div>`;
+    // ^to make my code less long I appended from the JS to the html. Instead of repeating that code block 9 times I just 
+    // made the rows the length of the array 
 });
+
+handleStore();
 
 const handleSave = () => {
     store = []; 
